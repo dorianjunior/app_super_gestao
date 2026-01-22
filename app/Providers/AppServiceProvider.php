f09f910045edeffeb836a5d17678b45a73f26a08
Dorian Costa <dorian.costa@sc.senac.br>
2026-01-22 19:28:36 -0300

    feat: ajustar comprimento padrão de strings para compatibilidade com MySQL mais antigo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        // Fix para MySQL mais antigo (InfinityFree)
+        // Limite de 1000 bytes para índices
+        Schema::defaultStringLength(191);
     }
 }
